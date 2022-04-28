642d26c7c6d87eca23f03ad3a56d09a26721cae0
SkylineFX <rrstudor@gmail.com>
2022-04-28 20:41:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const advice = document.querySelector(".advice");
+const id = document.querySelector(".advice-id");
+const randomizeBtn = document.querySelector(".random-btn");
+const diceImg = document.querySelector(".dice");
+
+const randomize = () => {
+    fetch('https://api.adviceslip.com/advice')
+        .then(obj => obj.json())
+        .then(data => {
+            id.textContent = `advice #${data.slip.id}`;
+            advice.textContent = data.slip.advice;  
+        });
+}
+
+randomizeBtn.addEventListener('click', randomize);
+
+randomizeBtn.addEventListener('click', () => {
+    diceImg.style.rotation('90deg');
+});
